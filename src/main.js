cd698e6a34ec5d680f5a4fbfc42cca957b2d772d
Geovane Araújo <geovane-araujo@outlook.com>
2022-01-17 22:43:54 -0300

    starter

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import PrimeVue from 'primevue/config'
+
+import 'primeflex/primeflex.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+app.use(store)
+
+app.use(PrimeVue, { ripple: true })
+
+app.use(router).mount('#app')
